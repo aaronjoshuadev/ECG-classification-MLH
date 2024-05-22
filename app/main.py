640288fbc667b57859d09453386d3ff69cc065fb
Aaron Joshua Dela Cruz <aaronjoshua.dlacruz@gmail.com>
2024-05-22 12:40:36 +0800

    Update 22-05-24 - update the login

--- app/main.py
+++ app/main.py
@@ -72,19 +72,20 @@ def creds_entered():
     else:
         st.session_state["authenticated"] = False
         st.error("Invalid Username/Password :face_with_raised_eyebrow:")
-def authenticate_user():
-        if "authenticated" in st.session_state:
-                st.text_input(label="Username: ", value="", key="user", on_change=creds_entered)
-                st.text_input(label="Password: ", value="", key="passwd", type="password", on_change=creds_entered)
-                return False
-        else:
-            if st.session_state["authenticated"]:
-                return True
-            else:
-                st.text_input(label="Username: ", value="", key="user", on_change=creds_entered)
-                st.text_input(label="Password: ", value="", key="passwd", type="password", on_change=creds_entered)
 
-                return False
+
+def authenticate_user():
+    if "authenticated" not in st.session_state:
+        st.session_state["authenticated"] = False
+
+    if st.session_state["authenticated"]:
+        return True
+    else:
+        st.text_input(label="Username: ", value="", key="user", on_change=creds_entered)
+        st.text_input(label="Password: ", value="", key="passwd", type="password", on_change=creds_entered)
+        return False
+
+
 if authenticate_user():
 
     @st.cache_resource
