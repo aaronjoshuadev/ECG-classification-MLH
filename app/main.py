f67f1b191464b99122190ad9f6562a7449469263
Aaron Joshua Dela Cruz <aaronjoshua.dlacruz@gmail.com>
2024-05-22 12:47:36 +0800

    Update 22-05-24 - update the login

--- app/main.py
+++ app/main.py
@@ -6,25 +6,18 @@ import scipy.io
 # from tensorflow import keras
 from src.visualization import plot_ecg
 
-def creds_entered():
-    if st.session_state["user"].strip() == "admin" and st.session_state["passwd"].strip() =="admin":
-        st.session_state["authenticated"] = True
-    else:
-        st.session_state["authenticated"] = False
-        st.error("Invalid Username/Password :face_with_raised_eyebrow:")
 def authenticate_user():
-        if "authenticated" in st.session_state:
-                st.text_input(label="Username: ", value="", key="user", on_change=creds_entered)
-                st.text_input(label="Password: ", value="", key="passwd", type="password", on_change=creds_entered)
-                return False
-        else:
-            if st.session_state["authenticated"]:
-                return True
-            else:
-                st.text_input(label="Username: ", value="", key="user", on_change=creds_entered)
-                st.text_input(label="Password: ", value="", key="passwd", type="password", on_change=creds_entered)
+    if "authenticated" not in st.session_state:
+        st.session_state["authenticated"] = False
 
-                return False
+    if st.session_state["authenticated"]:
+        return True
+    else:
+        st.text_input(label="Username: ", value="", key="user", on_change=creds_entered)
+        st.text_input(label="Password: ", value="", key="passwd", type="password", on_change=creds_entered)
+        return False
+
+
 if authenticate_user():
 #---------------------------------#
 # Page layout
